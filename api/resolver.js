8ff3fa2d9631d7b6f6c5e030c3d668cd7733b02e
Richard Siwady <rsiwady29@gmail.com>
2016-02-20 17:33:48 -0600

    Added resolver

--- api/resolver.js
+++ api/resolver.js
@@ -1,5 +1,22 @@
 module.exports = function(req, res) {
+  var url = req.query.url.trim();
+
+  var matches = url.match(/\-([0-9]+)(\?[a-zA-Z0-9=&]+)?$/);
+  if (!matches) {
+    res.status(400).send('Invalid URL format');
+    return;
+  }
+
+  var id     = matches[1].split('?')[0];
+  var width  = 600;
+  var height = 250;
+  var html   = "<div style='width:" + width + "px; text-align:left;' >" +
+               "<iframe  src='http://eventbrite.com/tickets-external?eid=" + id + "&ref=etckt' " +
+                        "frameborder='0' height='" + height + "px' width='100%' " +
+                        "vspace='0' hspace='0' marginheight='5' " + "marginwidth='5' " +
+                        "scrolling='auto' allowtransparency='true'>" +
+               "</iframe> </div>";
   res.json({
-    body: "hi"
+    body: html
   });
 };
